6b04f36fdd87f8cd354eb3c6f2f1f69803490171
Juan Manfrin Fernandes <juan.fernandes@grupointegrado.br>
2026-06-03 17:49:42 -0300

    aula4

--- aula4_persistindo_dados/src/db/database.ts
+++ aula4_persistindo_dados/src/db/database.ts
@@ -7,7 +7,7 @@ const db: Database.Database = new Database(p);
 
 db.exec( 
     `CREATE TABLE IF NOT EXISTS tasks (
-        id INT NOT NULL PRIMARY KEY,
+        id INTEGER NOT NULL PRIMARY KEY,
         title TEXT NOT NULL,
         done INTEGER NOT NULL DEFAULT 0,
         created_at TEXT DEFAULT CURRENT_TIMESTAMP
--- aula4_persistindo_dados/src/services/tasksService.ts
+++ aula4_persistindo_dados/src/services/tasksService.ts
@@ -1,35 +1,54 @@
+import db from "../db/database"
+
 export interface Task {
     id: number
     title: string
     done: boolean
+    created_at: string
 };
+
+interface TaskAsRow {
+    id: number
+    title: string
+    done: number
+    created_at: string
+};
+
+function toTask(taskRow: TaskAsRow): Task {
+    return {
+        ...taskRow,
+        done: taskRow.done === 1
+    };
+};
+
 
 let tasks: Task[] = [];
 let nextId = 1;
 
 // Função que lista todas as tarefas
 function getAll(): Task[] {
-    return tasks;
+    const rows = db.prepare(`SELECT * FROM tasks;`).all() as TaskAsRow[];
+    return rows.map(toTask);
 };
 
 // Função que lista tarefa por ID
 function getTaskById(id: number): Task | undefined {
-    const arrayTasks: Task[] = tasks;
-    const taskFound: Task | undefined = arrayTasks.find( (task) => { return task.id === id });
-    
-    return taskFound;
-};
+    const row = db.prepare(`SELECT * FROM tasks WHERE id = ?;`).get(id) as TaskAsRow;
+    if(!row) {
+        return undefined
+    };
+    return toTask(row)
+};  
 
 // Função que cria uma tarefa
-function createTask(title: string): Task | undefined {    
-    tasks.push({
-        id: nextId,
-        title: title,
-        done: false
-    });
-    const createdTask: Task | undefined  = tasks.find( task => task.id === nextId);
-    nextId++
-    return createdTask;
+function createTask(title: string): Task | undefined {
+    const stmt = db.prepare(`INSERT INTO tasks (title) VALUES (?);`);
+    const result = stmt.run(title);
+
+    const taskId = result.lastInsertRowid as number;
+    const newTask = getTaskById(taskId);
+
+    return newTask;
 };
 
 // Função que atualiza título da tarefa
@@ -40,14 +59,17 @@ function updateTask(id: number, newTitle: string ): Task | null {
         return null;
     };
 
-    foundTask.title = newTitle;
-    return foundTask;
+    const stmt = db.prepare(`UPDATE tasks SET title = ? WHERE id = ?;`);
+    stmt.run(newTitle, foundTask.id);
+
+    const updatedTask = getTaskById(foundTask.id);
+
+    return updatedTask!;
 
 }
-
 // Função que marca tarefa como concluída
 function doneTask(id: number) {
-    // implementar
+    
 }
 
 // Função que deleta uma tarefa
